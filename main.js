aa2ce4f2fda879685b5efea3103525c9179fbb11
Vasily Romanov <va5ily@MacBook-Air-Vasily.local>
2023-02-19 18:19:46 +0300

    fourth update

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const elements = {
-    info: 'info_inner'
+    info: 'info_inner',
+    weightAmount: 'weight_amount',
+    weightMeasure: 'weight_measure'
 }
 
+const weightOfPocket = [0.5, 2, 5]
+const measure = 'кг'
+
 document.querySelectorAll(`.${elements.info}`)
-    .forEach((item) => {
+    .forEach((item, index) => {
         item.style.backgroundImage = "url('images/Photo.png')"
+        item.querySelector(`.${elements.weightAmount}`).innerText = weightOfPocket[index]
+        item.querySelector(`.${elements.weightMeasure}`).innerText = measure
         // item.style.backgroundRepeat = "no-repeat"
     })
